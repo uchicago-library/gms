412e65fc2adf213b2b10c81daa8988e45ff0eff0
John Jung <john@fieldrotation.com>
2020-09-25 11:40:23 -0500

    Added UV.

--- view/index.php
+++ view/index.php
@@ -1,42 +1,57 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="uv/uv.css"/>
+        <script src="https://unpkg.com/resize-observer-polyfill@1.5.1/dist/ResizeObserver.js"></script>
+        <script type="text/javascript" src="uv/uv-assets/js/bundle.js"></script>
+        <script type="text/javascript" src="uv/uv-helpers.js"></script>
+        <script type="text/javascript" src="uv-dist-umd/UV.js"></script>
+        <title>Rose and Chess</title>
+        <style>
+            html, body {
+                 height: 100%;
+                 margin: 0;
+            }   
+            #uv {
+                width: 100%;
+                height: 100%;
+            }   
+        </style>
+    </head>
+    <body>
 
-require_once('cleaner.php');
-$clean = cleaner();
+        <div id="uv"></div>
 
-require_once('printer.php');
+        <script type="text/javascript">
+            function getUrlVars() {
+                var vars = {};
+                var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+                    vars[key] = value;
+                });
+                return vars;
+            }
 
-?>
-<html>
-  <head>
-    <title>The Goodspeed Manuscript Collection</title>
-  </head>
-  <style>
-    body {
-      margin: 0;
-    }
-  </style>
-  <body>
+            urlDataProvider = new UV.URLDataProvider();
+            var doc = getUrlVars()['doc'];
+            var manifest_uri = "https://iiif-manifest.lib.uchicago.edu/gms/" + doc + "/gms-" + doc + ".json";
+            //var manifest_uri = "https://iiif-manifest-dev.lib.uchicago.edu/gms-0019.json";
 
-  <iframe
-  src="<?php print_iiif_iframe_src(); ?>"
-  width="100%"
-  height="100%"
-  allowfullscreen
-  frameborder="0"></iframe>
+            if ('cv' in getUrlVars()) {
+                var cv = Number(urlDataProvider.get('cv', 0));
+            } else {
+                var cv = parseInt(getUrlVars()['obj']) - 1;
+            }
 
-<script type="text/javascript">
-// ASYNCHRONOUS GOOGLE ANALYTICS CODE.
-// http://code.google.com/apis/analytics/docs/tracking/asyncTracking.html
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-12414285-1']);
-_gaq.push(['_trackPageview']);
+            var uv = createUV("uv", {
+                manifestUri: manifest_uri,
+                assetsDir: "uv/uv-assets",
+                configUri: "/uv-config.json",
+                canvasIndex: cv
+            }, new UV.URLDataProvider());
+        </script>
 
-(function() {
-    var ga = document.createElement('script'); ga.type =
-'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' :
-'http://www') + '.google-analytics.com/ga.js';
-    (document.getElementsByTagName('head')[0] ||
-document.getElementsByTagName('body')[0]).appendChild(ga);
-})();
-</script>
+    </body>
+</html>
